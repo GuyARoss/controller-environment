c72b17301b37f2f07cfb78326f4cf33c961dd140
GuyARoss <guyallenross@gmail.com>
2021-01-29 04:06:25 -0500

    wip ipc

--- agent_framework/ipc/agent.py
+++ agent_framework/ipc/agent.py
@@ -1,5 +1,18 @@
-from ipc.base_ipc import BaseIPC
+from ipc.base_ipc import IPC_Instance
 
-class AgentIPC(BaseIPC):
-    def __init__(self):
+class AgentIPC:
+    '''
+    agent ipc data properties
+    - last processed: datetime the last frame was processed
+    - agent stats: dict of stats that will programatically be displayed on the overlay
+    - last action: controllerEventType
+    '''
+
+    def __init__(self, ipc_type: IPC_Instance):
+        self.ipc_instance = ipc_type(name='agent_ipc')
+
+    def update_properties(self, agent_stats: dict, action: str):
+        pass
+
+    def read(self):
         pass
