b19786b52857e180692300d60e596cecbcf32373
GuyARoss <guyallenross@gmail.com>
2021-01-29 02:33:55 -0500

    device commit

---
+++ scripts/usb-serial-viewer.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3.8
+import sys
+import serial, string
+import glob
+
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+print(serial_ports())
+
+# output = " "
+# ser = serial.Serial('/dev/ttyUSB0', 4800, 8, 'N', 1, timeout=1)
+
+# while True:
+#   print("----")
+#   while output != "":
+#     output = ser.readline()
+#     print(output)
+#   output = " "
